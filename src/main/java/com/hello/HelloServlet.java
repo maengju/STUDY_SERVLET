9bbd39bd48f9e8cecd159a699d91a00dad7e105d
maengju <maeng8588@gmail.com>
2021-09-13 17:29:36 +0900

    STUDY_SERVLET

---
+++ src/main/java/com/hello/HelloServlet.java
@@ -0,0 +1,29 @@
+package com.hello;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+//@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public void init() throws ServletException {
+		System.out.println("실행하자마자 제일 먼저 호출되는 함수 - init()");
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("클라이언트 요청이 있을 때 마다 실행된다 - service()");
+	}
+       
+	@Override
+	public void destroy() {
+		System.out.println("서블릿 종료될 때 호추로딘다 - destroy()");
+	}
+}
